06f59c0d2de706f4e4c734659c26a184e2b74711
wanglong <495250430@qq.com>
2019-02-21 23:20:15 +0800

    测试1

--- src/main/java/com/zll/blog/HelloController.java
+++ src/main/java/com/zll/blog/HelloController.java
@@ -1,15 +1,18 @@
 package com.zll.blog;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private StudentProperties studentProperties;
 
     @RequestMapping("/hello")
     public String hello(){
-        return "hello word!";
+        return "姓名："+studentProperties.getName() + ", 年龄"+studentProperties.getAge();
     }
 }
